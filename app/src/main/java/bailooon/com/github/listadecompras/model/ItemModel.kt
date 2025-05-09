da5e8388dfea64a378ca8f680c1c847d3c7be21e
bailooon <matheus@bailon.com.br>
2025-05-08 22:06:44 -0300

    criando o ItemModel

---
+++ app/src/main/java/bailooon/com/github/listadecompras/model/ItemModel.kt
@@ -0,0 +1,14 @@
+package bailooon.com.github.listadecompras.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+    @Entity
+    data class ItemModel(
+
+
+        @PrimaryKey(autoGenerate = true)
+        val id: Int = 0,
+        val name: String
+    )
